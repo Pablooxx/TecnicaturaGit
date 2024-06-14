703be32a69ca4bd78e0ffbac3f24d36c6242bc1c
Pablo Aparicio <apariciopablo99@gmail.com>
2024-06-13 23:33:49 -0300

    Clase-7-js

--- Javascript/Lección02/02-01-ConcatenaciondeCadenas.js
+++ Javascript/Lección02/02-01-ConcatenaciondeCadenas.js
@@ -14,3 +14,21 @@ console.log(juntos) // Contexto String o Contexto Cadena
 
 nombre += apellido; //Concatenamos usando el operador simplificado
 console.log(nombre);
+
+// hoy ya no se usa varm se utiliza let y const
+let nombre2 = 'Nicolas';
+console.log(nombre2);
+
+const apellido2 = 'Paez';
+//apellido2 = 'Paez'; una constante no puede ser modificada
+console.log(apellido2);
+
+let x,  y; // Se pueden crear varias variables dentro de una misma línea
+x = 18, y = 21; // Se puede hacer asignación de varias variables dentro de la misma línea
+let z = x + y; // Se asigna el valor de la operación
+console.log(z)
+
+ let _1num = 31; // No utilizar número como primer caracter de una variable
+ let rompiendo = 'rompe'; // No utilizar palabras reservadas para una variable
+ console.log(_1num);
+ console.log(rompiendo);
---
+++ Javascript/Lección03/03-01-OperadoresJavaScrip.js
@@ -0,0 +1,18 @@
+// Ejercicio para encontrar número pares e impares
+let parInpar = 36;
+if(parInpar % 2 == 0){
+    console.log('Es un número PAR')
+}
+else{
+    console.log('Es un número IMPAR')
+}
+
+// Ejercicio: es mauor de edad
+let edad = 10, adulto = 18;
+if( edad >= adulto){
+    console.log('Usted es una persona adulta')
+}
+else{
+    console.log('Usted es una persona menor de edad')
+}
+
