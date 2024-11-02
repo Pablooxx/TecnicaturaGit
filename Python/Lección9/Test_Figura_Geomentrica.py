d0b22ad3d8ff34ca59d8407a9c43027109abe5f8
PabloApa7 <89990249+Pablooxx@users.noreply.github.com>
2024-11-02 15:39:25 -0300

    Commit de Herencias Multiples Python
    realizamos Commit de sobre las herencias multiples en la lección9

---
+++ Python/Lección9/Test_Figura_Geomentrica.py
@@ -0,0 +1,19 @@
+from Cuadrado import Cuadrado
+from Rectangulo import Rectangulo
+
+cuadrado1 = Cuadrado (7, 'Rojo')
+print(cuadrado1.alto)
+print(cuadrado1.alto)
+print(f'cálculo del área del cuadrado: {cuadrado1.calcular_area()}')
+
+
+# MRO = Method Resolution Order
+print(Cuadrado.mro())
+
+print(cuadrado1)
+
+rectangulo1 = Rectangulo(3, 8, 'verde')
+print(f'Calculo del area del rectangulo: {rectangulo1.calcular_area()}')
+print(rectangulo1)
+
+
